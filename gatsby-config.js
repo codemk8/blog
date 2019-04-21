2d482c862f9c174f867a0ab7d35d4b1e7979c068
Yongpeng Zhang <ypzhang@pop-os.localdomain>
2019-04-20 22:15:37 -0400

    Add markdown example

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Code M's blog`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Codemk8's personal blog`,
+    author: `@codemk8`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
