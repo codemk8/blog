3326e0b1a5d8f1ddc4627dcc965a1bde924b82fb
Code M <codemk8@gmail.com>
2019-04-24 19:06:54 -0400

    Auto resize index

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,9 @@ import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import Media from "react-media"
+import Helmet from "react-helmet"
+import Sidebar from "./sidebar"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -27,17 +30,61 @@ const Layout = ({ children }) => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            Last built © {new Date().toJSON().slice(0,10).replace(/-/g,'/')}
-          </footer>
+      style={{
+        margin: "0 auto",
+        maxWidth: 980,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        height: "100%"
+      }}
+    >
+      <Media query={{ maxWidth: 848 }}>
+        {matches =>
+          matches ? (
+            <div
+              style={{
+                margin: "0 auto",
+                maxWidth: 980,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                height: "100%",
+                padding: "25px"
+              }}
+            >
+              <div style={{ flex: 1 }}>{children}</div>
+            </div>
+          ) : (
+            <div
+              style={{
+                margin: "0 auto",
+                maxWidth: 980,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                height: "100%",
+                padding: "25px"
+              }}
+            >
+              <div style={{ flex: 2.5, paddingRight: "30px" }}>
+                {children}
+              </div>
+            <div style={{ flex: 1 }}>
+                <Sidebar
+                  title="CodeMk8"
+                  description="Articles on coding: go-lang, microservice, cloud, C++ etc."
+                />
+                <Sidebar
+                  title="Email"
+                  description="codemk8 in gmail"
+                />
+              </div>
+            </div>
+          )
+        }
+      </Media>
+         
         </div>
       </>
     )}
@@ -48,4 +95,77 @@ Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default Layout
+export default Layout 
+
+/*
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet
+      title="Gatsby Default Starter"
+      meta={[
+        { name: "description", content: "Sample" },
+        { name: "keywords", content: "sample, something" }
+      ]}
+    />
+    <Header />
+    <div
+      style={{
+        margin: "0 auto",
+        maxWidth: 980,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        height: "100%"
+      }}
+    >
+      <Media query={{ maxWidth: 848 }}>
+        {matches =>
+          matches ? (
+            <div
+              style={{
+                margin: "0 auto",
+                maxWidth: 980,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                height: "100%",
+                padding: "25px"
+              }}
+            >
+              <div style={{ flex: 1 }}>{children()}</div>
+            </div>
+          ) : (
+            <div
+              style={{
+                margin: "0 auto",
+                maxWidth: 980,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                height: "100%",
+                padding: "25px"
+              }}
+            >
+              <div style={{ flex: 2.5, paddingRight: "30px" }}>
+                {children()}
+              </div>
+<div style={{ flex: 1 }}>
+                <Sidebar
+                  title="Codestack"
+                  description="Articles on React and Node.js. All articles are written by Me. Fullstack Web Development."
+                />
+                <Sidebar
+                  title="About author"
+                  description="I am a Full-stack Web Developer specializing in React and Node.js based in Nigeria."
+                />
+              </div>
+            </div>
+          )
+        }
+      </Media>
+    </div>
+  </div>
+);
+
+export default TemplateWrapper;
+*/
